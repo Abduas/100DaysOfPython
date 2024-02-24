0ffa7cb60470fa30fa76dabe16c05574c28fb30a
Abduas <4pa20cs009@pace.edu.in>
2024-02-24 15:08:19 +0530

    second commit

--- day8/greetfunc.py
+++ day8/greetfunc.py
@@ -1,5 +1,13 @@
-def greet():
-    print("welcome to our page")
-    print("how are you ")
-    print("what is your name")
-greet()
+# def greet():
+#     print("welcome to our page")
+#     print("how are you ")
+#     print("what is your name")
+# greet()
+
+def greet_with_name(name,location):
+    print(f"welcome to our page {name}")
+    print(f"how are you {name} ")
+    print(f"how is the wheather in {location}")
+
+
+greet_with_name("abdullah","kasaragod")
